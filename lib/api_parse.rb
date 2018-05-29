0b1e6955d7db5f539b84db38636c1997d502178c
Derek Chen <derekgc@icloud.com>
2018-05-29 13:54:23 -0400

    update api

--- lib/api_parse.rb
+++ lib/api_parse.rb
@@ -14,10 +14,10 @@ def parse_api(link)
 end
 
 
-def test
+def nyartbeat_parse(link)
   link = "http://www.nyartbeat.com/list/event_searchNear?latitude=40.719130&longitude=-73.980000&MaxResults=5&SortOrder=distance&free=1"
   s = Net::HTTP.get_response(URI.parse(link)).body
-  binding.pry
+  # binding.pry
   data = JSON.parse(Hash.from_xml(s).to_json)
 end
 
