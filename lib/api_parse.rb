5f18389bdb278855022a53236e0f8c281d8d3c36
Derek Chen <derekgc@icloud.com>
2018-05-29 14:46:26 -0400

    add geocode and nyartbeat parse

--- lib/api_parse.rb
+++ lib/api_parse.rb
@@ -9,16 +9,45 @@ require 'pry'
 def parse_api(link)
   data ={}
   raw_data = RestClient.get(link)
-  data = JSON.parse(drink_url_link)
+  data = JSON.parse(raw_data)
   # binding.pry
 end
 
 
-def nyartbeat_parse(link)
-  link = "http://www.nyartbeat.com/list/event_searchNear?latitude=40.719130&longitude=-73.980000&MaxResults=5&SortOrder=distance&free=1"
+def nyartbeat_parse(free=1)
+  geo= {}
+  geo['lat'] = 40.719130
+  geo['lng'] = -73.980000
+  link = "http://www.nyartbeat.com/list/event_searchNear?latitude=#{geo['lat']}&longitude=#{geo['lng']}&MaxResults=10&SortOrder=distance&free=#{free}"
   s = Net::HTTP.get_response(URI.parse(link)).body
-  # binding.pry
+  binding.pry
   data = JSON.parse(Hash.from_xml(s).to_json)
 end
 
-test
+def address_to_geo(address)
+  link = 'https://maps.googleapis.com/maps/api/geocode/json?address='
+  key= '&key=AIzaSyB8y9s45xVG7OAhCdYa14p80sQBEiKEgV8'
+  address = address.gsub(' ', '+')
+  link = link + address + key
+  # binding.pry
+  data = parse_api(link)
+  data['results'][0]['geometry']['location']
+end
+
+def geo_to_address(geo)
+  link = 'https://maps.googleapis.com/maps/api/geocode/json?latlng='
+  key= '&key=AIzaSyB8y9s45xVG7OAhCdYa14p80sQBEiKEgV8'
+  link = link + geo['lat'].to_s + ","+ geo['lng'].to_s + key
+  data = parse_api(link)
+  # binding.pry
+  data["results"][0]["formatted_address"]
+end
+
+geo= {}
+geo['lat'] = 40.719130
+geo['lng'] = -73.980000
+geo_to_address(geo)
+
+# address_to_geo("flatiron school, manhattan, new york")
+
+# nyartbeat_parse
