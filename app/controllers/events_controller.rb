9ee727b8758c92afbff4b71ebc1006013be52102
srajeev91 <github email address>
2018-05-29 14:36:43 -0400

    edited user and event views and controllers, added errors

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,2 +1,16 @@
 class EventsController < ApplicationController
+  before_action :get_event, only: [:show]
+  def index
+    @events = Event.all
+  end
+
+  def show
+
+  end
+
+  private
+
+  def get_event
+    @event = Event.find(params[:id])
+  end
 end
