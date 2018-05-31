64d6235c68ffe6d6d509a5c5ba0aaca5423f35c4
[Derek] <[derekgc@icloud.com]>
2018-05-30 22:19:23 -0400

    add eventbrite

--- lib/api_parse.rb
+++ lib/api_parse.rb
@@ -21,6 +21,18 @@ def nyartbeat_parse(geo , free=1)
   data = JSON.parse(Hash.from_xml(s).to_json)
 end
 
+def eventbrite(category="food", range="15mi")
+
+  api_key = 'TVBPW6RABWSC73XMYW5Y'
+
+  url = "https://www.eventbriteapi.com/v3/events/search/?expand=organizer,venue&location.address=NYC&location.address=Indore&expand=organizer,venue&start_date.keyword=this_week&location.within=#{range}&subcategories=10003&token=#{api_key}"
+  data = parse_api(url)
+  binding.pry
+  true
+end
+
+
+
 def address_to_geo(address)
   geo= {}
   geo['lat'] = 40.7319579
@@ -57,12 +69,14 @@ def geo_to_neighborhood(geo)
   n[0]["long_name"]
 end
 
-geo= {}
-geo['lat'] = 40.7319579
-geo['lng'] = -73.9768964
-puts geo_to_address(geo)
-puts geo_to_neighborhood(geo)
+eventbrite
 
-y = address_to_geo("18 Stuyvesant Oval, new york, new york")
-puts y
+# geo= {}
+# geo['lat'] = 40.7319579
+# geo['lng'] = -73.9768964
+# puts geo_to_address(geo)
+# puts geo_to_neighborhood(geo)
+#
+# y = address_to_geo("18 Stuyvesant Oval, new york, new york")
+# puts y
 # puts nyartbeat_parse(y, 0)
--- lib/eventbrite.rb
+++ lib/eventbrite.rb
@@ -1,69 +1,19 @@
-# require 'faraday'
-# require 'base64'
-# require 'json'
-# require "pry"
-#
-# latitude = '40.77493'
-# longitude = '-74.419415'
-# api_key = 'TVBPW6RABWSC73XMYW5Y'
-# max = '3'
-# date = 'This week'
-# city = "New York City"
-# within = '10013'
-#
-# urlencoded = URI.encode_www_form({'app_key' => api_key, 'within' => '10013', 'latitude' => latitude, 'longitude' => longitude, 'max' => '3', 'date' => date})
-#
+require 'faraday'
+require 'RestClient'
+require 'base64'
+require 'json'
+require "pry"
+
+
+
+
 # url = "https://www.eventbriteapi.com/v3/event/search/subcategories/10003/?token=TVBPW6RABWSC73XMYW5Y&location.within=10013"
-#
+
 # https://www.eventbriteapi.com/v3/events/search/?token=TVBPW6RABWSC73XMYW5Y&start_date.keyword=next_week&expand=venue&location.latitude=40.9353537&location.longitude=-74.8405065&location.within=110km
-# https://www.eventbriteapi.com/v3/events/search/?location.address=NYC&location.within=100km&subcategories=10003&token=TVBPW6RABWSC73XMYW5Y
-#
-# def parse_api(link)
-#   data ={}
-#   raw_data = RestClient.get(link)
-#   data = JSON.parse(raw_data)
-#   # binding.pry
-# end
-#
-#
-# events = parse_api(url)
-#
-# binding.pry
-# events.each do |event|
-#   if event.first.first == 'event'
-#     title = event['event']['title']
-#     if event['event']['venue'] == nil
-#       venue = ""
-#       address = ""
-#       city = ""
-#       region = ""
-#       postal_code = ""
-#       latitude = ""
-#       longitude = ""
-#     else
-#       venue = event['event']['venue']['name']
-#       address = event['event']['venue']['address']
-#       city = event['event']['venue']['city']
-#       region = event['event']['venue']['region']
-#       postal_code = event['event']['venue']['postal_code']
-#       latitude = event['event']['venue']['latitude']
-#       longitude = event['event']['venue']['longitude']
-#     end
-#     category = event['event']['category']
-#     start_date = event['event']['start_date']
-#     end_date = event['event']['end_date']
-#     price = event['event']['tickets'].first['ticket']['price']
-#
-#     p "Event:      #{title}"
-#     p "Venue:      #{venue}"
-#     p "Category:   #{category}"
-#     p "Start Date: #{start_date}"
-#     p "End Date:   #{end_date}"
-#     p "Price:      $#{price}"
-#     p "Address:    #{address}, #{city}, #{region}, #{postal_code}"
-#     p "Latitude:   #{latitude}"
-#     p "Longitude:  #{longitude}"
-#
-#     puts "\n"
-#   end
-# end
+
+def parse_api(link)
+  data ={}
+  raw_data = RestClient.get(link)
+  data = JSON.parse(raw_data)
+  # binding.pry
+end
