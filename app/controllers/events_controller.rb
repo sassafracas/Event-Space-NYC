1da9744351e0f597c1e11903a091356ecb09f8f6
Derek Chen <derekgc@icloud.com>
2018-06-01 10:53:01 -0400

    update the search Merge branch 'sang1' into derek1

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -14,6 +14,7 @@ class EventsController < ApplicationController
   end
 
   def show
+    @user = current_user
     @event.address = "NYC" if @event.address == nil
     @events =[]
     @events << @event
